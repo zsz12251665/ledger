45b7ae428946959ed885a20e9909d301a59c9faf
Chipmunk <22286934+zsz12251665@users.noreply.github.com>
2024-10-28 05:47:01 +0000

    refactor(Transaction): implement the `ReadonlySet<Entry>` interface

--- src/core/Transaction.ts
+++ src/core/Transaction.ts
@@ -3,25 +3,54 @@ import type { AccountLike } from './Account';
 import { Entry } from './Entry';
 import type { ValueLike } from './Value';
 
-export class Transaction {
+export class Transaction implements ReadonlySet<Entry> {
     date: DateTime;
     private _entries = new Set<Entry>();
-    get entries(): ReadonlySet<Entry> {
-        return this._entries;
+
+    get size() {
+        return this._entries.size;
     }
 
-    constructor(date: DateTime) {
-        this.date = date;
-        if (this.constructor === Transaction) Object.seal(this);
+    has(value: Entry) {
+        return this._entries.has(value);
     }
 
-    addEntry(account: AccountLike, value: ValueLike): Entry {
+    keys() {
+        return this._entries.keys();
+    }
+
+    values() {
+        return this._entries.values();
+    }
+
+    entries() {
+        return this._entries.entries();
+    }
+
+    [Symbol.iterator]() {
+        return this._entries.values();
+    }
+
+    forEach(callback: (value: Entry, value2: Entry, set: ReadonlySet<Entry>) => void, thisArg?: any) {
+        return this._entries.forEach(callback, thisArg);
+    }
+
+    add(account: AccountLike, value: ValueLike): Entry {
         const entry = new Entry(this, account, value);
         this._entries.add(entry);
         return entry;
     }
 
-    deleteEntry(entry: Entry) {
+    clear() {
+        return this._entries.clear();
+    }
+
+    delete(entry: Entry) {
         return this._entries.delete(entry);
+    }
+
+    constructor(date: DateTime) {
+        this.date = date;
+        if (this.constructor === Transaction) Object.seal(this);
     }
 }
